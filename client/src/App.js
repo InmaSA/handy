1ee9e970b186db0fff72a14dc1d12a5c5e0f573c
Inma <inmanewmail@gmail.com>
2019-08-18 19:34:21 +0200

    icono y logo creados y home page empezada

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Signup from './components/particular.components/Signup'
 
 function App() {
   return (
-    <>
+  <>
     <Switch>
        <Route path="/" exact component={Home} /> 
        <Route exact path='/particular/signup' render={() => <Signup/>}/> 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,16 +11,42 @@ class Home extends Component {
 
   render () {
     return (
-      <div className="container">
-          <h1>Iron Profile</h1>
+      <>
+        <header className="container hero">
+          <div className="row">
+              <div className="col-md-4">
+                  <img className="home-big-logo" src="../../images/Handy-logo.png" alt="Handy logo"></img>  
+              </div>
+              <div className="col-md-6">
+                  <h1>Conectando personas para la solución de problemas</h1>
+              </div>
+          </div>
+        </header>
 
-              <Link className="link" to="/particular/signup">Sign up</Link>
- 
 
-              <Link className="link" to="/particular/login">Log in</Link>
+        <div className="container">     
+
+          <div className="row home-row">
+            <div className="col-md-5 home-cards">
+              <h2>Si eres un particular, encuentra al profesional que buscas para el trabajo que necesitas</h2>
+
+              <Link className="button" to="/particular/login">Accede a tu cuenta</Link>
+              <p>¿Aún no eres usuario?</p>
+
+              <Link className="button" to="/particular/signup">Sign up</Link>
+
+            </div>
 
 
-      </div>
+            <div className="col-md-5 home-cards">
+
+                <h2>Aquí van los profesionales</h2>
+
+            </div>
+          </div>
+
+        </div>
+      </>  
     )
   }
 }
