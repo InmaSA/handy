c6940c5740d4f2883b0babe5a6d35cd829fd5e05
Inma <inmanewmail@gmail.com>
2019-08-20 18:21:20 +0200

    formulario de profesional terminado pero sin subir foto

--- client/src/components/professional.components/ProfSignup.js
+++ client/src/components/professional.components/ProfSignup.js
@@ -14,7 +14,8 @@ class ProfSignup extends Component {
 
     if (name === 'spain') {
       value = e.target.checked
-    }
+    } 
+    
     this.setState({ [name]: value })
   }
 
@@ -24,7 +25,7 @@ class ProfSignup extends Component {
     const {username, email, password, job, description, localities, spain} = this.state
     this.authServices.signupProf(username, email, password, job, description, localities, spain)
     .then((theNewUser) => {
-      this.setState({username:'', password: '', email: '', job: '', description: '', localities: '', spain: '' })
+      this.setState({username:'', password: '', email: '', job: '', description: '', localities: '', spain: false })
       this.props.setUser(theNewUser)
       this.props.history.push('/professional/profile')
     
@@ -46,14 +47,83 @@ class ProfSignup extends Component {
                     <label htmlFor="input-email">Email:</label>
                     <input type="text" name="email" id="input-email" value={this.state.email} onChange={this.handleChangeInput}></input>
                     
-                    <label htmlFor="input-job">¿Qué trabajo realizas?:</label>
-                    <input type="text" name="job" id="input-job" value={this.state.job} onChange={this.handleChangeInput}></input>
+                    <p>¿Qué trabajo realizas?:</p>
+                    <select name="job" id="input-job" selected={this.state.job} onChange={this.handleChangeInput}>
+                        <option value='albañilería'>albañilería</option>
+                        <option value='animador sociocultural'>animador sociocultural</option>
+                        <option value='catering'>catering</option>
+                        <option value='clases particulares'>clases particulares</option>
+                        <option value='costura'>costura</option>
+                        <option value='cuidado de mascotas'>cuidado de mascotas</option>
+                        <option value='cuidado de personas'>cuidado de personas</option>
+                        <option value='electricidad'>electricidad</option>
+                        <option value='fisioterapia'>fisioterapia</option>
+                        <option value='fontanería'>fontanería</option>
+                        <option value='guía turístico'>guía turístico</option>
+                        <option value='informática'>informática</option>
+                        <option value='jardinería'>jardinería</option>
+                        <option value='limpieza del hogar'>limpieza del hogar</option>
+                        <option value='maquillaje profesional'>maquillaje profesional</option>
+                        <option value='música y espectáculos'>música y espectáculos</option>
+                        <option value='peluquería y estética'>peluquería y estética</option>
+                        <option value='personal shopper'>personal shopper</option>
+                        <option value='pintura'>pintura</option>
+                        <option value='reformas'>reformas</option>
+                    </select>
                     
                     <label htmlFor="input-description">Describe tu trabajo brevemente: </label>
                     <input type="textaera" name="description" id="input-description" value={this.state.description} onChange={this.handleChangeInput}></input>
 
-                    <label htmlFor="input-localities">Provincias:</label>
-                    <input type="text" name="localities" id="input-localities" value={this.state.localities} onChange={this.handleChangeInput}></input>
+                    <p>Selecciona una provincia:</p>
+                    <select name="localities" id="input-localities" selected={this.state.localities} onChange={this.handleChangeInput}>
+                        <option value='Albacete'>Albacete</option>
+                        <option value='Alicante/Alacant'>Alicante/Alacant</option>
+                        <option value='Almería'>Almería</option>
+                        <option value='Araba/Álava'>Araba/Álava</option>
+                        <option value='Asturias'>Asturias</option>
+                        <option value='Ávila'>Ávila</option>
+                        <option value='Badajoz'>Badajoz</option>
+                        <option value='Barcelona'>Barcelona</option>
+                        <option value='Bizkaia'>Bizkaia</option>
+                        <option value='Burgos'>Burgos</option>
+                        <option value='Cáceres'>Cáceres</option>
+                        <option value='Cádiz'>Cadiz</option>
+                        <option value='Cantabria'>Cantabria</option>
+                        <option value='Castellón/Castelló'>Castellón/Castelló</option>
+                        <option value='Ciudad Real'>Ciudad Real</option>
+                        <option value='Córdoba'>Córdoba</option>
+                        <option value='Coruña, A'>Coruña, A</option>
+                        <option value='Cuenca'>Cuenca</option>
+                        <option value='Gipuzkoa'>	Gipuzkoa</option>
+                        <option value='Girona'>Girona</option>
+                        <option value='Granada'>Granada</option>
+                        <option value='Guadalajara'>Guadalajara</option>
+                        <option value='Huelva'>Huelva</option>
+                        <option value='Huesca'>Huesca</option>
+                        <option value='Jaén'>Jaén</option>
+                        <option value='León'>León</option>
+                        <option value='Lleida'>Lleida</option>
+                        <option value='Lugo'>Lugo</option>
+                        <option value='Madrid'>Madrid</option>
+                        <option value='Málaga'>Málaga</option>
+                        <option value='Murcia'>Murcia</option>
+                        <option value='Navarra'>Navarra</option>
+                        <option value='Ourense'>Ourense</option>
+                        <option value='Palencia'>	Palencia</option>
+                        <option value='Pontevedra'>Pontevedra</option>
+                        <option value='Rioja, La'>Rioja, La</option>
+                        <option value='Salamanca'>Salamanca</option>
+                        <option value='Segovia'>Segovia</option>
+                        <option value='Sevilla'>Sevilla</option>
+                        <option value='Soria'>Soria</option>
+                        <option value='Tarragona'>Tarragona</option>
+                        <option value='Teruel'>Teruel</option>
+                        <option value='Toledo'>Toledo</option>
+                        <option value='Valencia/València'>Valencia/València</option>
+                        <option value='Valladolid'>Valladolid</option>
+                        <option value='Zamora'>Zamora</option>
+                        <option value='Zaragoza'>	Zaragoza</option>
+                    </select>
                     
                     <p>¿Trabajarías en toda la península?:</p>
                     <label htmlFor="spain-yes">
@@ -74,5 +144,4 @@ class ProfSignup extends Component {
   }
 }
 
-export default ProfSignup;
-
+export default ProfSignup
