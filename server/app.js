e97d8a11a8b29af9995c831f79073d7b6807b8af
Inma <inmanewmail@gmail.com>
2019-08-28 12:08:47 +0200

    deploy4

--- server/app.js
+++ server/app.js
@@ -3,8 +3,6 @@ require('dotenv').config()
 const bodyParser   = require('body-parser')
 const cookieParser = require('cookie-parser')
 const express      = require('express')
-const favicon      = require('serve-favicon')
-const hbs          = require('hbs')
 
 
 const logger       = require('morgan')
@@ -56,9 +54,8 @@ app.use(cors(corsOptions))
 
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
 
 
 // Session passport settings
