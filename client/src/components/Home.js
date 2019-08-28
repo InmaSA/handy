ffdeb4c3e7a3fc540ff189c79145e13080c015ec
Inma <inmanewmail@gmail.com>
2019-08-28 18:08:36 +0200

    home-page maquetada

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AuthServices from '../services/auth.services'
 
+import HomeCarousel from './HomeCarousel'
+
 class Home extends Component {
 
   constructor () {
@@ -13,50 +15,66 @@ class Home extends Component {
 
   render () {
     return (
-      <>
+      <div className="home-page">
 
         <header className="container hero">
-          <div className="row">
-              <div className="col-md-4">
+          <div className="row ">
+              <div className="col-md-3">
                   <img className="home-big-logo" src="../../images/Handy-logo.png" alt="Handy logo"></img>  
               </div>
-              <div className="col-md-6">
-                  <h1>Conectando personas para la solución de problemas</h1>
+              <div className="col-md-5">
+                  <h1>Conectando personas para la solución de problemas.</h1>
               </div>
           </div>
         </header>
 
-
+        
         <div className="container">     
 
-          <div className="row home-row">
-            <div className="col-md-5 home-cards">
-              <h2>Si eres un particular, encuentra al profesional que buscas para el trabajo que necesitas</h2>
+          <div className="row justify-content-start">
+            <section className="col-sm-3 home-cards">
+              
+              <header>
+                 <h2>Si eres un particular, encuentra al profesional que buscas:</h2>
+              </header>
 
-              <Link className="button" to="/particular/login">Accede a tu cuenta</Link>
-              <p>¿Aún no eres usuario?</p>
+              <div className="access">
+                  <Link className="button" to="/particular/login">Accede a tu cuenta</Link>
+                  <p>¿Aún no eres usuario?</p>
 
-              <Link className="button" to="/particular/signup">Sign up</Link>
+                  <Link className="button" to="/particular/signup">Sign up</Link>
+              </div>
 
-            </div>
+            </section>
 
 
-            <div className="col-md-5 home-cards">
+            <section className="col-sm-3 home-cards">
 
-                <h2>Si eres un profesional, este es tu sitio:</h2>
+                <header>
+                    <h2>Si eres un profesional y quieres darte a conocer, este es tu sitio:</h2>
+                </header>
 
-                <Link className="button" to="/professional/login">Accede a tu cuenta</Link>
-                <p>¿Aún no eres usuario?</p>
+                <div className="access">
+                    <Link as="div" className="button" to="/professional/login">Accede a tu cuenta</Link>
+                    <p>¿Aún no eres usuario?</p>
 
-                <Link className="button" to="/professional/signup">Sign up</Link>
+                    <Link as="div" className="button" to="/professional/signup">Sign up</Link>
+                </div>
 
-            </div>
+            </section>
           </div>
 
         </div>
-      </>  
+
+            <div className="row justify-content-start home-carousel">
+               <HomeCarousel></HomeCarousel>
+            </div>
+
+      </div>  
     )
   }
 }
 
 export default Home
+
+
