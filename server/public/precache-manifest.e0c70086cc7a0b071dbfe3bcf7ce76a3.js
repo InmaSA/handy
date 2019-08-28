fe971573301b743ac26781a7fc6a9248681f1ab9
Inma <inmanewmail@gmail.com>
2019-08-28 12:02:59 +0200

    deploy3

---
+++ server/public/precache-manifest.e0c70086cc7a0b071dbfe3bcf7ce76a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc95c41177615b8299bb2089eb3dc769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a13dd32c184e98f345ac",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "768a717c6f4fe3beca83",
+    "url": "/static/css/main.6adb538d.chunk.css"
+  },
+  {
+    "revision": "a13dd32c184e98f345ac",
+    "url": "/static/js/2.5651febc.chunk.js"
+  },
+  {
+    "revision": "768a717c6f4fe3beca83",
+    "url": "/static/js/main.2fa1e78a.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
